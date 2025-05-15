ef6a2c02bda61fad09efb4a126836b4b5705e36c
inoninja <88498417+inoninja@users.noreply.github.com>
2025-05-16 03:40:45 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://qydffarro:TofuEats1@cluster0.q1owwie.mongodb.net/animeShop';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://qydffarro:TofuEats1@cluster0.q1owwie.mongodb.net';
     const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
