729e360bb8259962430120df860598d77f1b3c64
inoninja <88498417+inoninja@users.noreply.github.com>
2025-05-16 04:33:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,19 +16,12 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS
-app.use(cors());
+app.use(cors({
+  origin: ['https://anime-you-one.vercel.app', 'http://localhost:3000']
+}));
 
 // Routes
 app.use('/api/products', require('./routes/productRoutes'));
-
-// Set static folder in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
-     
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
