b48fd60e48cb09b8ff6947807219d99afdfb19c9
inoninja <88498417+inoninja@users.noreply.github.com>
2025-05-16 16:29:47 +0800

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,7 +7,7 @@ const Product = require('../models/Product');
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, './uploads/');
   },
   filename: function(req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
